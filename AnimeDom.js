424266c7b9897d3e04b0e6837b4f65bc2f67b276
andrey.voropay <avoropay@ukr.net>
2016-09-22 07:56:20 +0300

    Bubble & Selection Sort

---
+++ AnimeDom.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Andrey.Voropay on 9/16/2016.
+ */
+
+$(document).ready(function() {
+
+	$("#randomButton").click(function () {
+		fillArray();
+		$("#ArrayBubble [id^='array']").each(function (index, item) {
+			$(item).width(ArrayBubble[index]);
+		});
+		$("#ArraySelection [id^='array']").each(function (index, item) {
+			$(item).width(ArraySelection[index]);
+		});
+		$("#ArrayQuick [id^='array']").each(function (index, item) {
+			$(item).width(ArrayQuick[index]);
+		});
+	});
+
+	$("#startButton").click(function () {
+		setTimeout(BubbleSort(ArrayBubble), 10);
+		setTimeout(SelectionSort(ArraySelection), 10);
+	});
+});
---
+++ Sort.js
@@ -0,0 +1,106 @@
+/**
+ * Created by Andrey.Voropay on 9/16/2016.
+ */
+var ArrayBubble = new Array(25);
+var ArraySelection = new Array(25);
+var ArrayQuick = new Array(25);
+var waitRendering = false;
+
+function fillArray() {
+	var index = 0;
+	do {
+		var rnd = Math.ceil(Math.random() * 100);
+		if (ArrayBubble.indexOf(rnd) == -1) {
+			ArrayBubble[index] = rnd;
+			ArraySelection[index] = rnd;
+			ArrayQuick[index] = rnd;
+			index++;
+		}
+	} while (index <= 25);
+
+}
+
+function VisualSwap(A, B, I, J, id) {
+
+	var nodeA = $("#"+ id + I),
+		nodeB = $("#"+ id + J);
+	nodeA.toggleClass("barHswop", true);
+	nodeB.toggleClass("barHswop", true);
+
+	nodeA.width(B);
+	nodeB.width(A);
+
+	nodeA.toggleClass("barHswop", false);
+	nodeB.toggleClass("barHswop", false);
+}
+
+function BubbleSort(A) {
+	var T, I, J, arrayLegnth = A.length + 1;
+	for (I = arrayLegnth; I > 0; I--) {
+		for (J = 0; J <= arrayLegnth - 1; J++) {
+			if (A[J] > A[J + 1]) {
+				VisualSwap(A[J], A[J + 1], J, J + 1, "array1_row");
+				T = A[J];
+				A[J] = A[J + 1];
+				A[J+1] = T;
+			}
+		}
+	}
+}
+
+function SelectionSort(A) {
+	var I, J, T, arrayLegnth = A.length + 1;
+	for (I = 0; I <= arrayLegnth; I++) {
+		for (J = arrayLegnth; J >= (I + 1); J--) {
+			if (A[I] > A[J]) {
+				VisualSwap(A[I], A[J], I, J, "array2_row");
+				T = A[I];
+				A[I] = A[J];
+				A[J] = T;
+			}
+		}
+	}
+}
+
+/*function QuickSort(A, iLo, iHi) {
+var   Lo, Hi, Pivot, T;
+	{   Lo = iLo;   Hi = iHi;
+	Pivot = A[(Lo + Hi) div 2];
+	do
+		while (A[Lo] < Pivot) { Lo++;
+		while (A[Hi] > Pivot) { Hi--;
+		if (Lo <= Hi) {
+		{
+			T = A[Lo];
+			A[Lo] = A[Hi];
+			A[Hi] = T;
+			Lo++;
+			Hi--;
+		}
+	 (Lo > Hi);
+	if (Hi > iLo) { QuickSort(A, iLo, Hi) };
+	if (Lo < iHi) { QuickSort(A, Lo, iHi) };
+end;
+
+/*
+ procedure QuickSort(var A: array of Integer; iLo, iHi: Integer) ;
+ var   Lo, Hi, Pivot, T: Integer;
+ begin   Lo := iLo;   Hi := iHi;
+ Pivot := A[(Lo + Hi) div 2];
+ repeat
+ while A[Lo] < Pivot do Inc(Lo) ;
+ while A[Hi] > Pivot do Dec(Hi) ;
+ if Lo <= Hi then
+ begin
+        T := A[Lo];
+        A[Lo] := A[Hi];
+        A[Hi] := T;
+        Inc(Lo) ;
+        Dec(Hi) ;
+  end;
+  until Lo > Hi;
+  if Hi > iLo then QuickSort(A, iLo, Hi) ;
+  if Lo < iHi then QuickSort(A, Lo, iHi) ;
+  end;
+ */
+
